1f33b1169b7387e3bb870d8ea9e37c187eefa934
Ash <redcoolike@gmail.com>
2024-11-23 14:06:52 +0300

    classSettings

--- src/index.js
+++ src/index.js
@@ -6,11 +6,31 @@ class Settings {
             ['difficulty', 'easy']
         ])
         this.values = new Map([
-            ['theme', new Map(['light', true], ['gray', true])],
-            ['music', new Map(['pop', true], ['rock', true], ['chillout', true], ['off', true])],
-            ['difficulty', new Map(['normal', true], ['hard', true], ['nightmare', true])]
+            ['theme', new Map([['light', true], ['gray', true]])],
+            ['music', new Map([['pop', true], ['rock', true], ['chillout', true], ['off', true]])],
+            ['difficulty', new Map([['normal', true], ['hard', true], ['nightmare', true]])]
         ]);
-        this.userSettings = {};
+
+      
+        this.userSettings = new Map();
+    }
+    setSettings(key, value){
+        if(!this.values.has(key)){
+            throw new Error('Ошибка')
+        }
+
+        if(!this.values.get(key).has(value)){
+            throw new Error('такой темы нет')
+        }
+
+        this.userSettings.set(key, value);
     }
 
+    get settings(){
+        const newMapValues = new Map(this.defaultSettings)
+        for(let [key, value] of this.userSettings){
+            newMapValues.set(key, value);
+        }
+        return Object.fromEntries(newMapValues);
+    }
 }
